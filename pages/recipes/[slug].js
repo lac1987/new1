98a651551455a352209be98ac86999e58d00aaa9
lac1987 <lacpc1987@gmail.com>
2021-09-07 22:25:47 +0800

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -30,6 +30,16 @@ export async function getStaticProps({params}) {
      content_type: 'recipe',
      'fields.slug': params.slug
    })
+
+   if (!items.length){
+     return {
+       redirect:{
+         destination: '/',
+         permanent: false
+       }
+     }
+   }
+
    return {
      props: {recipe: items[0]},
      revalidate: 1
@@ -37,7 +47,7 @@ export async function getStaticProps({params}) {
 }
 
 export default function RecipeDetails({recipe}) {
-  
+
   if (!recipe) return <Skeleton/>
   
   const {featuredImage, title, cookingTime, ingredients, method} = recipe.fields
